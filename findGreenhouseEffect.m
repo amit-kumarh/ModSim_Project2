79646ab527390af6a05f076c35c77472d36827b7
Amit Kumar <amitkh@gmail.com>
2021-11-01 15:25:43 -0400

    demonstrated 3 equilibriums

--- findGreenhouseEffect.m
+++ findGreenhouseEffect.m
@@ -2,6 +2,6 @@ function res = findGreenhouseEffect(temp)
     S = 1367.6;
     sigma = 5.67e-8;
     Q = S/4;
-    alpha = .3;
+    alpha = findAlbedo(temp);
     res = (Q*(1-alpha))/(sigma*temp^4);
 end
